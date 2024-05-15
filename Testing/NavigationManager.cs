13d9de8fa2f6a77a8cecdf1aad86bc480d9bd221
JoonaV556 <joona.virmajoki@gmail.com>
2024-05-15 18:34:50 +0300

    Script folder cleanup

--- Testing/NavigationManager.cs
+++ Testing/NavigationManager.cs
@@ -8,32 +8,37 @@ public class NavigationManager : MonoBehaviour
     // NavigationManager is responsible for updating the navigation grid during runtime 
 
     #region OnEnableDisable
-    private void OnEnable() {
-        ObstacleHealth.OnObstacleDestroyed += UpdateNavGridNearObstacle;
+    private void OnEnable()
+    {
+        //ObstacleHealth.OnObstacleDestroyed += UpdateNavGridNearObstacle;
     }
-    private void OnDisable() {
-        ObstacleHealth.OnObstacleDestroyed -= UpdateNavGridNearObstacle;
+    private void OnDisable()
+    {
+        // ObstacleHealth.OnObstacleDestroyed -= UpdateNavGridNearObstacle;
     }
     #endregion
 
     /// <summary>
     /// Update the navigation grid (faster)
     /// </summary>
-    public static void UpdateNavGrid() {
+    public static void UpdateNavGrid()
+    {
 
     }
 
     /// <summary>
     /// Recalculate the whole navigation grid (takes more time)
     /// </summary>
-    public static void RescanNavGrid() { 
+    public static void RescanNavGrid()
+    {
 
     }
 
     /// <summary>
     /// Update the navigation grid near a specific collider (Use for position targeted updates - really fast)
     /// </summary>
-    public static void UpdateNavGridNearObstacle(Collider2D Collider) {
+    public static void UpdateNavGridNearObstacle(Collider2D Collider)
+    {
         AstarPath.active.UpdateGraphs(Collider.bounds);
     }
 }
