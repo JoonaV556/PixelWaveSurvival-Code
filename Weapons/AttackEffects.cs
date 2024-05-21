cb823d643b9aed48258b59ae0761cbfd684d187a
JoonaV556 <joona.virmajoki@gmail.com>
2024-05-21 15:20:26 +0300

    Moved attackeffects to separate file

---
+++ Weapons/AttackEffects.cs
@@ -0,0 +1,62 @@
+namespace AttackEffects
+{
+    using UnityEngine;
+    using System;
+
+    [Serializable]
+    public abstract class AttackEffect
+    {
+        public GameObject targetObject;
+    }
+
+    public class KnockbackEffect : AttackEffect
+    {
+        /// <summary>
+        /// Attempts to apply knockback to target object by adding force to its Rigidbody2D component.
+        /// </summary>
+        /// <param name="targetObject"></param>
+        /// <param name="knockbackForce"></param>
+        /// <param name="knockbackDirection">Direction to apply the knockback towards. Does not need to be normalized</param>
+        /// <param name="disableMovementDuration">Duration to disable movement on target object in seconds</param>
+        public KnockbackEffect(GameObject targetObject, float knockbackForce, Vector2 knockbackDirection, float disableMovementDuration = 0f)
+        {
+            targetObject.TryGetComponent(out Rigidbody2D rb);
+
+            bool canApply = rb != null;
+
+            if (canApply)
+            {
+                // Disable movement to enable adding rigidbody force
+                if (disableMovementDuration > 0)
+                {
+                    DisableMovementOnTarget(targetObject, disableMovementDuration);
+                }
+
+                float scaledKnockBackForce = knockbackForce * rb.mass;
+
+                // Apply knockback to target object
+                Debug.Log("Applied knockback to target object: " + targetObject.name);
+                rb.AddForce(knockbackDirection.normalized * scaledKnockBackForce, ForceMode2D.Impulse);
+            }
+        }
+
+        private void DisableMovementOnTarget(GameObject targetObject, float disableMovementDuration)
+        {
+            targetObject.TryGetComponent(out CharacterMovement movement);
+            if (movement != null)
+            {
+                // Prevent clamping velocity while knockback is applied
+                movement.ClampVelocity = false;
+                // Stop clamping velocity after duration
+                movement.StartCoroutine(EnableMovementOnTarget(movement, disableMovementDuration));
+            }
+        }
+
+        private IEnumerator EnableMovementOnTarget(CharacterMovement movement, float disableMovementDuration)
+        {
+            yield return new WaitForSeconds(disableMovementDuration);
+            movement.ClampVelocity = true;
+        }
+    }
+}
+
--- Weapons/ZombieAttack.cs
+++ Weapons/ZombieAttack.cs
@@ -16,6 +16,7 @@ public class ZombieAttack : MonoBehaviour
     public float disableMovementDurationOnKnockback = 0.9f;
 
     public float hitCooldown = 1f; // Minimum time in seconds between body hits
+    public float attackCooldown = 1f; // Minimum time in seconds between attacks
 
     Coroutine hitCooldownCoroutine;
 
@@ -53,67 +54,5 @@ public class ZombieAttack : MonoBehaviour
     {
         yield return new WaitForSeconds(hitCooldown);
         hitCooldownCoroutine = null;
-    }
-}
-
-// \/\/\/ Refactor to separate file \/\/\/
-namespace AttackEffects
-{
-    using UnityEngine;
-
-    [Serializable]
-    public abstract class AttackEffect
-    {
-        public GameObject targetObject;
-    }
-
-    public class KnockbackEffect : AttackEffect
-    {
-        /// <summary>
-        /// Applies knockback to target object
-        /// </summary>
-        /// <param name="targetObject"></param>
-        /// <param name="knockbackForce"></param>
-        /// <param name="knockbackDirection">Direction to apply the knockback towards. Does not need to be normalized</param>
-        /// <param name="disableMovementDuration">Duration to disable movement on target object in seconds</param>
-        public KnockbackEffect(GameObject targetObject, float knockbackForce, Vector2 knockbackDirection, float disableMovementDuration = 0f)
-        {
-            targetObject.TryGetComponent(out Rigidbody2D rb);
-
-            bool canApply = rb != null;
-
-            if (canApply)
-            {
-                // Disable movement to enable adding rigidbody force
-                if (disableMovementDuration > 0)
-                {
-                    DisableMovementOnTarget(targetObject, disableMovementDuration);
-                }
-
-                float scaledKnockBackForce = knockbackForce * rb.mass;
-
-                // Apply knockback to target object
-                Debug.Log("Applied knockback to target object: " + targetObject.name);
-                rb.AddForce(knockbackDirection.normalized * scaledKnockBackForce, ForceMode2D.Impulse);
-            }
-        }
-
-        private void DisableMovementOnTarget(GameObject targetObject, float disableMovementDuration)
-        {
-            targetObject.TryGetComponent(out CharacterMovement movement);
-            if (movement != null)
-            {
-                // Prevent clamping velocity while knockback is applied
-                movement.ClampVelocity = false;
-                // Stop clamping velocity after duration
-                movement.StartCoroutine(EnableMovementOnTarget(movement, disableMovementDuration));
-            }
-        }
-
-        private IEnumerator EnableMovementOnTarget(CharacterMovement movement, float disableMovementDuration)
-        {
-            yield return new WaitForSeconds(disableMovementDuration);
-            movement.ClampVelocity = true;
-        }
     }
 }
