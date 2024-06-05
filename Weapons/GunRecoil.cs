30c8398b35f09df33d082f3a4c9ab7d6af2bec50
JoonaV556 <joona.virmajoki@gmail.com>
2024-06-05 19:10:44 +0300

    WIP - Added recoil pulldown force (Gun returns back to aim direction)

--- Weapons/GunRecoil.cs
+++ Weapons/GunRecoil.cs
@@ -1,23 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GunRecoil : MonoBehaviour
 {
+    // Predefined public parameters are tailored for MP5 or other small SMGs
     public Transform RecoilPivot;
 
+    [Tooltip("How many degrees the gun kicks upwards per shot fired.")]
+    public float KickPerShot = 2f;
+    [Tooltip("How many degrees the gun can kick upwards at most. Relative to 0 degrees on z axis.")]
+    public float KickDegreesUpperLimit = 30f;
 
-    public Vector3 pivotRotationEulerAngles = Vector3.zero; // Remove later
+    [Tooltip("How fast gun kicks up")]
+    [Range(0, 1)] public float RecoilSlerpAlpha = 0.99f;
+    [Tooltip("How fast gun returns back to original rotation after recoil.")]
+    [Range(0, 1)] public float PullBackAlpha = 0.28f;
 
+    public bool UseRecoilMultiplier = true;
+    public bool UsePullbackMultiplier = true;
 
-    public float KickPerShot = 0.1f; // Recoil kick in degrees
-    public float MaxKickDegrees = 30f; // Gun cannot recoil upwards more than this
-    [Range(0, 1)] public float RecoilSlerpAlpha = 0.98f; // How fast gun kicks
+    [Tooltip("Optional multiplier for the alpha values")]
+    public float RecoilAlphaMultiplier = 180f;
+    [Tooltip("Optional multiplier for the alpha values")]
+    public float PullbackAlphaMultiplier = 180f;
 
-    public bool UseMultiplier = false;
-    public float RecoilAlphaMultiplier = 1f; // Gun kick speed multiplier, diminishing returns if too high
+    [Tooltip("How much time has to pass (in seconds) since last shot fired, before gun starts to pull back down to original rotation")]
+    public float PullDownDelayLength = 0.1f;
 
-    public Vector3 targetRot = Vector3.zero; // Make private later
+    Vector3 targetRot = Vector3.zero; // Make private later
 
     private void Start()
     {
@@ -35,11 +44,10 @@ public class GunRecoil : MonoBehaviour
     }
 
     float pendingKick = 0f;
-    float recoilControlCooldown = 0f;
-    public float RecoilControlCooldown = 0.3f;
+    float secondsBeforePulldown = 0f;
     private void AddKick(Firearm firearm)
     {
-        recoilControlCooldown = 0.3f;
+        secondsBeforePulldown = PullDownDelayLength;
         // Simulate kick by offsetting target rotation by degrees
         switch (WeaponSpriteController.CurrentLookSide)
         {
@@ -58,34 +66,66 @@ public class GunRecoil : MonoBehaviour
     {
         // Get rotation data
         var currentRot = RecoilPivot.transform.localRotation.eulerAngles;
-        targetRot = new Vector3(currentRot.x, currentRot.y, currentRot.z + pendingKick);
 
-        // Clamp target rotation based on current look side
-        switch (WeaponSpriteController.CurrentLookSide)
+        // Recoil upwards if shots fired, else pull weapon back down
+        bool shouldPullDown = secondsBeforePulldown == 0f;
+        if (shouldPullDown)
         {
-            case WeaponSpriteController.LookSide.Left:
-                targetRot = new Vector3(targetRot.x, targetRot.y, Mathf.Clamp(targetRot.z, -MaxKickDegrees, 0f));
-                break;
-            case WeaponSpriteController.LookSide.Right:
-                targetRot = new Vector3(targetRot.x, targetRot.y, Mathf.Clamp(targetRot.z, 0f, MaxKickDegrees));
-                break;
-        }
-
-        // Consume pending kick
-        pendingKick = 0f;
-
-        // Calculate slerp alpha
-        float timedAlpha;
-        if (UseMultiplier)
-        {
-            timedAlpha = RecoilSlerpAlpha * Time.deltaTime * RecoilAlphaMultiplier;
+            targetRot = new Vector3(currentRot.x, currentRot.y, 0f); // Target rotation is always 0 on z axis when pulling down
         }
         else
         {
-            timedAlpha = RecoilSlerpAlpha * Time.deltaTime;
+            // Add pending kick (degrees) to target rotation
+            targetRot = new Vector3(currentRot.x, currentRot.y, currentRot.z + pendingKick);
+            // Clamp target rotation based on current look side (Prevents gun from recoiling infinitely and doing 360) 
+            switch (WeaponSpriteController.CurrentLookSide)
+            {
+                case WeaponSpriteController.LookSide.Left:
+                    targetRot = new Vector3(targetRot.x, targetRot.y, Mathf.Clamp(targetRot.z, -KickDegreesUpperLimit, 0f));
+                    break;
+                case WeaponSpriteController.LookSide.Right:
+                    targetRot = new Vector3(targetRot.x, targetRot.y, Mathf.Clamp(targetRot.z, 0f, KickDegreesUpperLimit));
+                    break;
+            }
         }
 
-        // Add recoil by slerping towards target rotation
+        // Consume pending recoil kick
+        pendingKick = 0f;
+
+        // Reduce cooldown if there is any
+        if (secondsBeforePulldown > 0f)
+        {
+            secondsBeforePulldown = Mathf.Clamp(secondsBeforePulldown - Time.deltaTime, 0f, PullDownDelayLength);
+        }
+
+        // Calculate slerp alpha
+        float timedAlpha;
+        if (shouldPullDown)
+        {
+            // For pullback slerp
+            if (UsePullbackMultiplier)
+            {
+                timedAlpha = PullBackAlpha * Time.deltaTime * PullbackAlphaMultiplier;
+            }
+            else
+            {
+                timedAlpha = PullBackAlpha * Time.deltaTime;
+            }
+        }
+        else
+        {
+            // For recoil slerp
+            if (UseRecoilMultiplier)
+            {
+                timedAlpha = RecoilSlerpAlpha * Time.deltaTime * RecoilAlphaMultiplier;
+            }
+            else
+            {
+                timedAlpha = RecoilSlerpAlpha * Time.deltaTime;
+            }
+        }
+
+        // Add recoil or pullback by slerping towards target rotation
         RecoilPivot.localEulerAngles = Vector3.Slerp(currentRot, targetRot, timedAlpha);
     }
 }
